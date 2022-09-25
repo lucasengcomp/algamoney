328c818b6e95cfd8751a97b253d1dd4528d03756
Lucas Galvao <lucas.engcomp@outlook.com>
2022-09-24 23:10:41 -0300

    :sparkles: Listando todas as Categorias. Adicionado Flyway para versionamento de banco

---
+++ backend/src/main/java/com/algamoneyapi/repository/CategoriaRepository.java
@@ -0,0 +1,7 @@
+package com.algamoneyapi.repository;
+
+import com.algamoneyapi.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+}
