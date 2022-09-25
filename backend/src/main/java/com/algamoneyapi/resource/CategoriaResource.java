4e0e78e5f356582028c9b096adb3bdf51c246536
Lucas Galvao <lucas.engcomp@outlook.com>
2022-09-25 11:35:04 -0300

    :sparkles: Método para salvar Categoria

--- backend/src/main/java/com/algamoneyapi/resource/CategoriaResource.java
+++ backend/src/main/java/com/algamoneyapi/resource/CategoriaResource.java
@@ -3,10 +3,14 @@ package com.algamoneyapi.resource;
 import com.algamoneyapi.model.Categoria;
 import com.algamoneyapi.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriBuilder;
 
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -20,5 +24,17 @@ public class CategoriaResource {
     @GetMapping
     public List<Categoria> listarTodasCategorias() {
         return repository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Categoria> salvar(@RequestBody Categoria entidade, HttpServletResponse response) {
+        Categoria categoriaSalva = repository.save(entidade);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequestUri().path("/{codigo}")
+                .buildAndExpand(categoriaSalva.getCodigo()).toUri();
+        response.setHeader("Location", uri.toASCIIString());
+
+        return ResponseEntity.created(uri).body(categoriaSalva);
     }
 }
