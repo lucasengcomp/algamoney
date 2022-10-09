34af950cb2a70b523fd4f09a3b667803afccba97
Lucas Galvao <lucas.engcomp@outlook.com>
2022-10-09 14:35:35 -0300

    :sparkles: `feat` Adicionado método para criar Pessoa

--- backend/src/main/java/com/algamoneyapi/resource/PessoaResource.java
+++ backend/src/main/java/com/algamoneyapi/resource/PessoaResource.java
@@ -3,11 +3,15 @@ package com.algamoneyapi.resource;
 import com.algamoneyapi.model.Pessoa;
 import com.algamoneyapi.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/pessoas")
@@ -15,9 +19,24 @@ public class PessoaResource {
 
     @Autowired
     private PessoaRepository pessoaRepository;
-
     @GetMapping
     public List<Pessoa> listar() {
         return pessoaRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Pessoa> criar(@Validated @RequestBody Pessoa pessoa, HttpServletResponse response) {
+        Pessoa pessoaSalva = pessoaRepository.save(pessoa);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+                .buildAndExpand(pessoaSalva.getCodigo()).toUri();
+        response.setHeader("Location", uri.toASCIIString());
+
+        return ResponseEntity.created(uri).body(pessoaSalva);
+    }
+
+    @GetMapping("/{codigo}")
+    public Optional<Pessoa> buscarPeloCodigo(@PathVariable Long codigo) {
+        return pessoaRepository.findById(codigo);
     }
 }
